bd6177d51922a84b1c6ffeca71207976d48c3ea1
Martin Segura Dominguez <martin.segura_d@hotmail.com>
2016-11-19 11:23:52 -0500

    pocos cambios ..... revisar.....

--- app/src/main/java/com/example/martin/proyectorezero/NuevoActivity.java
+++ app/src/main/java/com/example/martin/proyectorezero/NuevoActivity.java
@@ -9,14 +9,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestHandle;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
 public class NuevoActivity extends Activity {
 
     TextView lbl1;
     EditText edt1, edt2;
     Button btn1, btn2;
 
-    String contenido = "";
     String codigo = "";
+    String mensaje = "";
+
+    ArrayList estado = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +51,56 @@ public class NuevoActivity extends Activity {
         String mensajeResivido = intentrecibido.getStringExtra("TipoAccion");
 
 
-        ValidarAccion(mensajeResivido, intentrecibido);
+        switch (mensajeResivido) {
+
+            case "modificar":
+
+                lbl1.setText("ESTA PANTALLA SERA PARA MODIFICAR");
+
+                edt1.setHint(intentrecibido.getStringExtra("Codigo") + "-> :V");
+                edt2.setText(" ");
+                edt2.setFocusable(true);
+
+                codigo = intentrecibido.getStringExtra("Codigo");
+
+                mensaje = "";
+
+                btn2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        mensaje = edt2.getText().toString();
+
+                        metodoModificar(codigo, mensaje);
+
+                    }
+                });
+
+
+                break;
+
+            case "agregar":
+
+                lbl1.setText("ESTA PANTALLA ES PARA GUARDAR");
+                edt1.setHint("Campo Codigo Autogenerado....");
+                edt2.setFocusable(true);
+
+                btn2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        metodo1();
+
+                    }
+                });
+
+                break;
+
+            default:
+
+                Toast.makeText(getApplicationContext(), "NO LLEGO PARAMETRO...", Toast.LENGTH_SHORT).show();
+
+        }
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -53,46 +115,6 @@ public class NuevoActivity extends Activity {
 
     }
 
-    public void ValidarAccion(String mensajeResivido, Intent intent) {
-
-        if (mensajeResivido.equals("modificar")) {
-
-            lbl1.setText("ESTA PANTALLA SERA PARA MODIFICAR");
-
-            edt1.setHint(intent.getStringExtra("Codigo") + "-> :V");
-            edt2.setText(" ");
-            edt2.setFocusable(true);
-
-
-            codigo = intent.getStringExtra("Codigo");
-
-
-            btn2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    //aun no puedo capturar el valor del campo edt2 , solucionar ....
-                    metodo2(codigo, "");
-                }
-            });
-
-
-        } else {
-
-            lbl1.setText("ESTA PANTALLA ES PARA GUARDAR");
-            edt1.setHint("Campo inhabilitado....");
-            edt2.setFocusable(true);
-
-
-            btn2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    metodo1();
-                }
-            });
-
-        }
-    }
 
     public void Salir() {
 
@@ -105,8 +127,62 @@ public class NuevoActivity extends Activity {
         Toast.makeText(getApplicationContext(), "HOLA GUARDAR", Toast.LENGTH_SHORT).show();
     }
 
-    public void metodo2(String codigo, String contenidocampo) {
+    public void metodoModificar(String codigo, String contenidocampo) {
+
+        estado.clear();
+
         Toast.makeText(getApplicationContext(), "LOS DATOS SON :" + codigo + " y " + contenidocampo, Toast.LENGTH_SHORT).show();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        String url = "http://examenfinal2016.esy.es/PROYECTOWEBSERVICE/CONTROLADOR/SexoControlador.php?op=4";
+
+        RequestParams params = new RequestParams();
+        params.add("codsexo", codigo);
+        params.add("nombsex", contenidocampo);
+
+        RequestHandle post = client.post(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                if (statusCode == 200) {
+
+                    try {
+
+                        JSONArray jsonArray = new JSONArray(new String(responseBody));
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+
+                            estado.add(jsonArray.getJSONObject(i).getString("estado"));
+
+                        }
+
+                        if (estado.get(0).toString() == "success") {
+
+                            Toast.makeText(getApplicationContext(), " SE ACTUALIZO CORRECTAMENTE ", Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(), " NO SE ACTUALIZO :V", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    } catch (Exception ex) {
+
+                        Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                Toast.makeText(getApplicationContext(), " OCURRIO ALGO EN EL PROCESO... ", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
